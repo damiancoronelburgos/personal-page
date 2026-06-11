b2f9a87b49671fd756d804fdf028880148a25eb4
damianCoronel <danielcoronelburgos1993@gmail.com>
2026-06-11 17:24:07 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/personal-page/',
+  base: '/',
   build: {
     outDir: 'docs',
   },
